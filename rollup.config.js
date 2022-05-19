5a89257ef9e860cedc3d5987452bffd66085616c
HarryXiong24 <harryxiong24@gmail.com>
2022-05-19 23:06:51 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -31,8 +31,20 @@ export default {
   input: './src/main.ts', // 入口文件
   output: [
     {
-      file: './dist/bundle.js', // 打包后的存放文件
+      file: './dist/bundle.cjs.js', // 打包后的存放文件
       format: 'cjs', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
+      name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
+      sourcemap: true, // 生成 bundle.map.js 文件，方便调试
+    },
+    {
+      file: './dist/bundle.es.js', // 打包后的存放文件
+      format: 'es', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
+      name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
+      sourcemap: true, // 生成 bundle.map.js 文件，方便调试
+    },
+    {
+      file: './dist/bundle.umd.js', // 打包后的存放文件
+      format: 'umd', // 输出格式  "amd", "cjs", "system", "es", "iife", "umd".
       name: 'bundleName', // 如果 iife, umd 需要指定一个全局变量
       sourcemap: true, // 生成 bundle.map.js 文件，方便调试
     },
